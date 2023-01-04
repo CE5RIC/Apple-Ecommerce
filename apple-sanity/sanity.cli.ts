a730cff0b9c102e684d3d9e6739e09746e3d5fee
CE5RIC <cedricdierckx5@hotmail.com>
2023-01-04 11:51:22 +0100

    Sanity folder created

---
+++ apple-sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'pj2ksk6c',
+    dataset: 'production'
+  }
+})
--- postcss.config.js
+++ postcss.config.js
@@ -3,4 +3,13 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
