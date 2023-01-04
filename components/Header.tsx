dd14a374d4d21b3d62f262c5bdcfc2117d4f6be2
CE5RIC <cedricdierckx5@hotmail.com>
2023-01-04 15:13:09 +0100

    header navbar created with Material UI

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,94 @@
-import React from "react";
-import Image from "next/legacy/image";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import Avatar from "@mui/material/Avatar";
+import { Tooltip } from "@mui/material";
+import React, { useState } from "react";
+import AppleIcon from "@mui/icons-material/Apple";
 
-function Header() {
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: "#555555",
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "#555555",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+export default function Header() {
   return (
-    <header>
-      <div>
-        <Image
-          src={"https://rb.gy/vsvv2o"}
-          layout="fill"
-          objectFit="contain"
-          alt={"Apple Logo"}
-        />
-      </div>
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{ backgroundColor: "#555555" }} position="static">
+        <Toolbar>
+          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+            <AppleIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", sm: "block", color: "#00000" },
+            }}
+          >
+            Products
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Tooltip title="Open settings">
+            <IconButton sx={{ p: 2 }}>
+              <Avatar alt="" src="" />
+            </IconButton>
+          </Tooltip>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
-
-export default Header;
