a9b7d289d7ccfb9db468a5bcc393dca9ab1721ac
CE5RIC <cedricdierckx5@hotmail.com>
2023-01-05 12:05:40 +0100

    button component and interface props created

---
+++ components/Button.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/system";
+import React from "react";
+
+interface Props {
+  title: string;
+}
+
+function Button({ title }: Props) {
+  return (
+    <>
+      <Box></Box>
+    </>
+  );
+}
+
+export default Button;
