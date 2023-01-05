da25f60c34cb1feca0ed2fd3f3aba50a875ba818
CE5RIC <cedricdierckx5@hotmail.com>
2023-01-05 11:56:31 +0100

    added som more typography and button to landing page

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,7 +1,28 @@
+import { Box, Button, Link, Paper } from "@mui/material";
 import React from "react";
 
 function Landing() {
-  return <div></div>;
+  return (
+    <>
+      <Box>
+        <h1>Iphone 14</h1>
+        <h2>Big and bigger.</h2>
+      </Box>
+
+      <Box>
+        <Button variant="contained" title="Buy Now">
+          Buy Now
+        </Button>
+        <Link>Learn More</Link>
+      </Box>
+
+      <Box>
+        <Paper variant="outlined">
+          <img src="" />
+        </Paper>
+      </Box>
+    </>
+  );
 }
 
 export default Landing;
