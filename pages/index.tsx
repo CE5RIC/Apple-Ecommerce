0bbc4e92096cff41c7db5c09ff9a96195cdf57b0
CE5RIC <cedricdierckx5@hotmail.com>
2023-01-06 15:39:59 +0100

    categories in props

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,6 +59,8 @@ export default Home;
 export const getServerSideProps: GetServerSideProps = async () => {
   const categories = await fetchCategories();
   return {
-    props: {},
+    props: {
+      categories,
+    },
   };
 };
