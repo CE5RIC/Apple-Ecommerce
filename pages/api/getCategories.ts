1cbf9dbc53eaa4766accdaeab8bc5a8cc1b95e4f
CE5RIC <cedricdierckx5@hotmail.com>
2023-01-10 14:25:56 +0100

    deleted import

--- pages/api/getCategories.ts
+++ pages/api/getCategories.ts
@@ -3,12 +3,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { groq } from 'next-sanity';
 import { sanityClient } from '../../sanity';
 
+
+// Groq gives us syntax highlighting
 const query = groq`*[_type == "category"] {
     _id,
     ...
 }`;
 
-// custo
+
 
 type Data = {
   categories: Category[]
@@ -20,4 +22,6 @@ export default async function handler(
 ) {
   
  const categories = await sanityClient.fetch(query) 
+console.log(categories);
+ res.status(200).json({categories});
 }
