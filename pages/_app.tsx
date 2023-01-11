8988ff8ce275d8cb681134046ed32350381a2cd3
CE5RIC <cedricdierckx5@hotmail.com>
2023-01-11 12:41:20 +0100

    toaster imported

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import { Store } from "@reduxjs/toolkit";
 import { store } from "../redux/store";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Toaster />
       <Component {...pageProps} />
     </Provider>
   );
