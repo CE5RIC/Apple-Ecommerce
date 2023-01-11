2f835a7665535f6b0c3e7f1e1e43ccc0f66456f5
CE5RIC <cedricdierckx5@hotmail.com>
2023-01-11 12:06:22 +0100

    removeFromBasket reducer added

--- redux/basketSlice.ts
+++ redux/basketSlice.ts
@@ -17,11 +17,31 @@ export const basketSlice = createSlice({
     addToBasket: (state: BasketState, action: PayloadAction<Device>) => {
         state.items = [...state.items, action.payload];
       },
-    
-  }
-})
+      removeFromBasket: (
+        state: BasketState,
+        action: PayloadAction<{ id: string }>
+      ) => {
+        const index = state.items.findIndex(
+          (item: Device) => item._id === action.payload.id
+        );
+  
+        let newBasket = [...state.items];
+  
+        if (index >= 0) {
+          newBasket.splice(index, 1);
+        } else {
+          console.log(
+            `Cant remove product (id: ${action.payload.id}) as its not in basket!`
+          );
+        }
+  
+        state.items = newBasket;
+      },
+    },
+  });
+  
 
 // Action creators are generated for each case reducer function
-export const { addToBasket} = basketSlice.actions
+export const { addToBasket, removeFromBasket} = basketSlice.actions
 
 export default basketSlice.reducer
